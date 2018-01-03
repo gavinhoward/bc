25a9d2e9fa3b9c44bee5bf07f3ed4496eeb8e4b5
Gavin Howard <yzena.tech@gmail.com>
2018-01-03 12:49:53 -0700

    Start working on the lexer

---
+++ arbprec/tiny-bc/lex.c
@@ -0,0 +1,165 @@
+#include <ctype.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "lex.h"
+
+static BcLexStatus bc_lex_whitespace(BcLex* lex, BcLexToken* token);
+static BcLexStatus bc_lex_string(BcLex* lex, BcLexToken* token);
+
+BcLexStatus bc_lex_init(BcLex* lex, const char* text) {
+
+	// Check for error.
+	if (lex == NULL) {
+		return BC_LEX_STATUS_INVALID_PARAM;
+	}
+
+	// Set the data.
+	lex->buffer = text;
+	lex->idx = 0;
+
+	return BC_LEX_STATUS_SUCCESS;
+}
+
+BcLexStatus bc_lex_next(BcLex* lex, BcLexToken* token) {
+
+	BcLexStatus status = BC_LEX_STATUS_SUCCESS;
+
+	// Get the character.
+	char c = lex->buffer[lex->idx];
+
+	// Increment the index.
+	++lex->idx;
+
+	char c2;
+
+	// This is the workhorse of the lexer.
+	switch (c) {
+
+		case '\0':
+			token->type = BC_LEX_EOF;
+			break;
+
+		case '\t':
+			status = bc_lex_whitespace(lex, token);
+			break;
+
+		case '\n':
+			token->type = BC_LEX_NEWLINE;
+			break;
+
+		case '\v':
+		case '\f':
+		case '\r':
+		case ' ':
+			status = bc_lex_whitespace(lex, token);
+			break;
+
+		case '!':
+			// Get the next character.
+			c2 = lex->buffer[lex->idx];
+
+			// The only valid way to have a bang is a NOT_EQ token.
+			// If it's not valid, we do not want to increment twice.
+			if (c2 == '=') {
+				++lex->idx;
+				token->type = BC_LEX_OP_REL_NOT_EQ;
+			}
+			else {
+				token->type = BC_LEX_INVALID;
+				status = BC_LEX_STATUS_INVALID_TOKEN;
+			}
+
+			break;
+
+		case '"':
+			status = bc_lex_string(lex, token);
+			break;
+
+		case '%':
+			// Get the next character.
+			c2 = lex->buffer[lex->idx];
+
+			// This character can either be alone or as an assignment.
+			// If it's an assignment, we need to increment the index.
+			if (c2 == '=') {
+				++lex->idx;
+				token->type = BC_LEX_OP_ASSIGN_MODULUS;
+			}
+			else {
+				token->type = BC_LEX_OP_MODULUS;
+			}
+
+			break;
+
+		default:
+
+			// All other characters are invalid.
+			token->type = BC_LEX_INVALID;
+			status = BC_LEX_STATUS_INVALID_TOKEN;
+			break;
+	}
+
+	return status;
+}
+
+static BcLexStatus bc_lex_whitespace(BcLex* lex, BcLexToken* token) {
+
+	token->type = BC_LEX_WHITESPACE;
+
+	// Get the character.
+	char c = lex->buffer[lex->idx];
+
+	// Eat all whitespace (and non-newline) characters.
+	while (isspace(c) && c != '\n') {
+		++lex->idx;
+		c = lex->buffer[lex->idx];
+	}
+
+	return BC_LEX_STATUS_SUCCESS;
+}
+
+static BcLexStatus bc_lex_string(BcLex* lex, BcLexToken* token) {
+
+	// TODO: Handle backslashes and other string weirdness.
+
+	// Set the token type.
+	token->type = BC_LEX_STRING;
+
+	// Get the starting index and character.
+	size_t i = lex->idx;
+	char c = lex->buffer[i];
+
+	// Find the end of the string, one way or the other.
+	while (c != '"' && c != '\0') {
+		c = lex->buffer[++i];
+	}
+
+	// If we have reached the end of the buffer, complain.
+	if (c == '\0') {
+		lex->idx = i;
+		return BC_LEX_STATUS_NO_STRING_END;
+	}
+
+	// Calculate the length of the string.
+	size_t len = i - lex->idx;
+
+	// Allocate the string.
+	token->data.string = malloc(len + 1);
+
+	// Check for error.
+	if (token->data.string == NULL) {
+		return BC_LEX_STATUS_MALLOC_FAIL;
+	}
+
+	// Copy the string.
+	token->data.string[0] = '\0';
+	strncpy(token->data.string, lex->buffer + lex->idx, len);
+
+	// Set the index. We need to go one
+	// past because of the closing quote.
+	lex->idx = i + 1;
+
+	return BC_LEX_STATUS_SUCCESS;
+}
---
+++ arbprec/tiny-bc/lex.h
@@ -0,0 +1,100 @@
+#include <stdlib.h>
+
+typedef enum BcLexStatus {
+
+	BC_LEX_STATUS_SUCCESS,
+
+	BC_LEX_STATUS_INVALID_PARAM,
+
+	BC_LEX_STATUS_MALLOC_FAIL,
+
+	BC_LEX_STATUS_INVALID_TOKEN,
+
+	BC_LEX_STATUS_NO_STRING_END,
+
+} BcLexStatus;
+
+typedef enum BcLexTokenType {
+
+	BC_LEX_OP_INC,
+	BC_LEX_OP_DEC,
+
+	BC_LEX_OP_NEG,
+
+	BC_LEX_OP_POWER,
+
+	BC_LEX_OP_MULTIPLY,
+	BC_LEX_OP_DIVIDE,
+	BC_LEX_OP_MODULUS,
+
+	BC_LEX_OP_PLUS,
+	BC_LEX_OP_MINUS,
+
+	BC_LEX_OP_ASSIGN,
+	BC_LEX_OP_ASSIGN_PLUS,
+	BC_LEX_OP_ASSIGN_MINUS,
+	BC_LEX_OP_ASSIGN_MULTIPLY,
+	BC_LEX_OP_ASSIGN_DIVIDE,
+	BC_LEX_OP_ASSIGN_MODULUS,
+	BC_LEX_OP_ASSIGN_POWER,
+
+	BC_LEX_OP_REL_EQUAL,
+	BC_LEX_OP_REL_LESS_EQ,
+	BC_LEX_OP_REL_GREATER_EQ,
+	BC_LEX_OP_REL_NOT_EQ,
+	BC_LEX_OP_REL_LESS,
+	BC_LEX_OP_REL_GREATER,
+
+	BC_LEX_NEWLINE,
+
+	BC_LEX_WHITESPACE,
+
+	BC_LEX_STRING,
+	BC_LEX_LETTER,
+	BC_LEX_NUMBER,
+
+	BC_LEX_KEY_DEFINE,
+	BC_LEX_KEY_BREAK,
+	BC_LEX_KEY_QUIT,
+	BC_LEX_KEY_LENGTH,
+	BC_LEX_KEY_RETURN,
+	BC_LEX_KEY_FOR,
+	BC_LEX_KEY_IF,
+	BC_LEX_KEY_WHILE,
+	BC_LEX_KEY_SQRT,
+	BC_LEX_KEY_SCALE,
+	BC_LEX_KEY_IBASE,
+	BC_LEX_KEY_OBASE,
+	BC_LEX_KEY_AUTO,
+
+	BC_LEX_EOF,
+
+	BC_LEX_INVALID,
+
+} BcLexTokenType;
+
+typedef union BcLexData {
+
+	char* string;
+
+} BcLexData;
+
+typedef struct BcLexToken {
+
+	BcLexTokenType type;
+	BcLexData data;
+
+} BcLexToken;
+
+typedef struct BcLex {
+
+	const char* buffer;
+	size_t idx;
+
+} BcLex;
+
+BcLexStatus bc_lex_init(BcLex* lex, const char* text);
+
+BcLexStatus bc_lex_next(BcLex* lex, BcLexToken* token);
+
+
