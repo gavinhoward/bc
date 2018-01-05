bf6e015fc4c1d4230e688b2c923347192e4efc7d
Gavin Howard <yzena.tech@gmail.com>
2018-01-05 11:24:11 -0700

    More work on parsing

--- arbprec/tiny-bc/parse.c
+++ arbprec/tiny-bc/parse.c
@@ -3,18 +3,22 @@
 
 #include "parse.h"
 
-static BcStatus bc_parse_preinc(BcParse* parse);
+static BcStatus bc_parse_func(BcParse* parse, BcStmt* stmt);
+static BcStatus bc_parse_stmt(BcParse* parse, BcStmt* stmt);
+static BcStatus bc_parse_expr(BcParse* parse, BcStmt* stmt);
+static BcStatus bc_parse_name(BcParse* parse, BcStmt* stmt);
+static BcStatus bc_parse_call(BcParse* parse);
+static BcStatus bc_parse_params(BcParse* parse);
 static BcStatus bc_parse_expandStack(BcParse* parse);
 
-BcStatus bc_parse_init(BcParse* parse, BcProgram* program, const char* text) {
+BcStatus bc_parse_init(BcParse* parse, BcProgram* program) {
 
 	// Check for error.
-	if (parse == NULL || program == NULL || text == NULL) {
+	if (parse == NULL || program == NULL) {
 		return BC_STATUS_INVALID_PARAM;
 	}
 
 	// Set the data.
-	bc_lex_init(&parse->lex, text);
 	parse->program = program;
 
 	// Malloc a stack.
@@ -32,180 +36,62 @@ BcStatus bc_parse_init(BcParse* parse, BcProgram* program, const char* text) {
 	return BC_STATUS_SUCCESS;
 }
 
-BcStatus bc_parse_parse(BcParse* parse) {
+BcStatus bc_parse_text(BcParse* parse, const char* text) {
 
-	// TODO: Finish this function.
+	// Check for error.
+	if (parse == NULL || text == NULL) {
+		return BC_STATUS_INVALID_PARAM;
+	}
+
+	// Init the lexer.
+	return bc_lex_init(&parse->lex, text);
+}
+
+BcStatus bc_parse_parse(BcParse* parse) {
 
 	// Check for error.
 	if (parse == NULL) {
 		return BC_STATUS_INVALID_PARAM;
 	}
 
-	BcStatus status;
+
+
+	BcStmt stmt;
 
 	// Get the next token.
-	status = bc_lex_next(&parse->lex, &parse->token);
+	BcStatus status = bc_lex_next(&parse->lex, &parse->token);
 
-	while (status == BC_STATUS_SUCCESS) {
+	// Check for error.
+	if (status != BC_STATUS_SUCCESS) {
+		return status;
+	}
 
-		switch (parse->token.type) {
+	switch (parse->token.type) {
 
-			case BC_LEX_OP_INC:
-			case BC_LEX_OP_DEC:
-			{
-				break;
-			}
-
-			case BC_LEX_OP_MINUS:
-			{
-				break;
-			}
-
-			case BC_LEX_OP_BOOL_NOT:
-			{
-				break;
-			}
-
-			case BC_LEX_NEWLINE:
-			{
-				break;
-			}
-
-			case BC_LEX_LEFT_PAREN:
-			{
-				break;
-			}
-
-			case BC_LEX_SEMICOLON:
-			{
-				break;
-			}
-
-			case BC_LEX_STRING:
-			{
-				break;
-			}
-
-			case BC_LEX_NAME:
-			{
-				break;
-			}
-
-			case BC_LEX_NUMBER:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_BREAK:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_CONTINUE:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_DEFINE:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_FOR:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_HALT:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_IBASE:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_IF:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_LAST:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_LENGTH:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_LIMITS:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_OBASE:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_PRINT:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_QUIT:
-			{
-				exit(0);
-				break;
-			}
-
-			case BC_LEX_KEY_READ:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_RETURN:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_SCALE:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_SQRT:
-			{
-				break;
-			}
-
-			case BC_LEX_KEY_WHILE:
-			{
-				break;
-			}
-
-			case BC_LEX_EOF:
-			{
-				return status;
-			}
-
-			default:
-			{
-				status = BC_STATUS_PARSE_INVALID_TOKEN;
-				break;
-			}
-		}
-
-		// Break on error.
-		if (status != BC_STATUS_SUCCESS) {
+		case BC_LEX_NEWLINE:
+		{
+			// We don't do anything if there is a newline.
 			break;
 		}
 
-		// Get the next token.
-		status = bc_lex_next(&parse->lex, &parse->token);
+		case BC_LEX_KEY_DEFINE:
+		{
+			// Check for error.
+			if (!BC_PARSE_CAN_EXEC(parse)) {
+				return BC_STATUS_PARSE_INVALID_TOKEN;
+			}
+
+			// Parse a function definition.
+			status = bc_parse_func(parse, &stmt);
+
+			break;
+		}
+
+		default:
+		{
+			status = bc_parse_stmt(parse, &stmt);
+			break;
+		}
 	}
 
 	return status;
@@ -229,7 +115,22 @@ void bc_parse_free(BcParse* parse) {
 	}
 }
 
-static BcStatus bc_parse_preinc(BcParse* parse) {
+static BcStatus bc_parse_func(BcParse* parse, BcStmt* stmt) {
+
+	// TODO: Add to the symbol table.
+
+
+}
+
+static BcStatus bc_parse_stmt(BcParse* parse, BcStmt* stmt) {
+
+}
+
+static BcStatus bc_parse_expr(BcParse* parse, BcStmt* stmt) {
+
+}
+
+static BcStatus bc_parse_name(BcParse* parse, BcStmt* stmt) {
 
 	BcStatus status = bc_lex_next(&parse->lex, &parse->token);
 
@@ -240,26 +141,81 @@ static BcStatus bc_parse_preinc(BcParse* parse) {
 	switch (parse->token.type) {
 
 		case BC_LEX_NAME:
+		{
+			stmt->type = BC_STMT_EXPR_NAME;
+			stmt->data.string = parse->token.data.string;
 			break;
+		}
 
 		case BC_LEX_KEY_IBASE:
+		{
+			stmt->type = BC_STMT_EXPR_IBASE;
 			break;
+		}
 
 		case BC_LEX_KEY_LAST:
+		{
+			stmt->type = BC_STMT_EXPR_LAST;
 			break;
+		}
 
 		case BC_LEX_KEY_OBASE:
+		{
+			stmt->type = BC_STMT_EXPR_OBASE;
 			break;
+		}
 
 		case BC_LEX_KEY_SCALE:
+		{
+			stmt->type = BC_STMT_EXPR_SCALE;
 			break;
+		}
 
 		default:
+		{
 			status = BC_STATUS_PARSE_INVALID_TOKEN;
 			break;
+		}
 	}
 
 	return status;
+}
+
+static BcStatus bc_parse_call(BcParse* parse) {
+
+	BcStatus status = bc_lex_next(&parse->lex, &parse->token);
+	if (status != BC_STATUS_SUCCESS) {
+		return status;
+	}
+
+	if (parse->token.type != BC_LEX_NAME) {
+		return BC_STATUS_PARSE_INVALID_TOKEN;
+	}
+
+	// Get the name of the function.
+	char* name = parse->token.data.string;
+
+	status = bc_lex_next(&parse->lex, &parse->token);
+	if (status != BC_STATUS_SUCCESS) {
+		return status;
+	}
+
+	if (parse->token.type != BC_LEX_LEFT_PAREN) {
+		return BC_STATUS_PARSE_INVALID_TOKEN;
+	}
+
+	status = bc_parse_params(parse);
+	if (status != BC_STATUS_SUCCESS) {
+		return status;
+	}
+
+	if (parse->token.type != BC_LEX_RIGHT_PAREN) {
+		return BC_STATUS_PARSE_INVALID_TOKEN;
+	}
+}
+
+static BcStatus bc_parse_params(BcParse* parse) {
+
 }
 
 static BcStatus bc_parse_expandStack(BcParse* parse) {
--- arbprec/tiny-bc/parse.h
+++ arbprec/tiny-bc/parse.h
@@ -8,15 +8,36 @@
 
 #define BC_PARSE_STACK_START (32)
 
-#define BC_PARSE_FLAG_FUNC (0x01)
+#define BC_PARSE_FLAG_FUNC_INNER (0x01)
 
-#define BC_PARSE_IN_FUNC(parse)  \
+#define BC_PARSE_FUNC_INNER(parse)  \
+	((parse)->stack[(parse)->stack_len - 1] & BC_PARSE_FLAG_FUNC_INNER)
+
+#define BC_PARSE_FLAG_FUNC (0x02)
+
+#define BC_PARSE_FUNC(parse)  \
 	((parse)->stack[(parse)->stack_len - 1] & BC_PARSE_FLAG_FUNC)
 
-#define BC_PARSE_FLAG_LOOP (0x02)
+#define BC_PARSE_FLAG_HEADER (0x04)
 
-#define BC_PARSE_IN_LOOP(parse)  \
-	((parse)->stack[(parse)->stack_len - 1] & BC_PARSE_FLAG_LOOP)
+#define BC_PARSE_HEADER(parse)  \
+	((parse)->stack[(parse)->stack_len - 1] & BC_PARSE_FLAG_HEADER)
+
+#define BC_PARSE_FLAG_LINE_END (0x08)
+
+#define BC_PARSE_LINE_END(parse)  \
+	((parse)->stack[(parse)->stack_len - 1] & BC_PARSE_FLAG_LINE_END)
+
+#define BC_PARSE_FLAG_LOOP_INNER (0x10)
+
+#define BC_PARSE_LOOP_INNER(parse)  \
+	((parse)->stack[(parse)->stack_len - 1] & BC_PARSE_FLAG_LOOP_INNER)
+
+#define BC_PARSE_CAN_EXEC(parse)  \
+	(!((parse)->stack[(parse)->stack_len - 1] & (BC_PARSE_FLAG_FUNC_INNER |  \
+	                                             BC_PARSE_FLAG_FUNC |        \
+	                                             BC_PARSE_FLAG_HEADER |      \
+	                                             BC_PARSE_FLAG_LOOP_INNER)))
 
 typedef enum BcParseStatus {
 
@@ -33,9 +54,12 @@ typedef struct BcParse {
 	uint32_t stack_len;
 	uint8_t* stack;
 
+	BcStmtList* inner;
+
 } BcParse;
 
-BcStatus bc_parse_init(BcParse* parse, BcProgram* program, const char* text);
+BcStatus bc_parse_init(BcParse* parse, BcProgram* program);
+BcStatus bc_parse_text(BcParse* parse, const char* text);
 
 BcStatus bc_parse_parse(BcParse* parse);
 
