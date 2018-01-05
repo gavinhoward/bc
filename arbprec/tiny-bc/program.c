47a527d5e930842530db6360c59badd31a09c7a0
Gavin Howard <yzena.tech@gmail.com>
2018-01-05 11:18:55 -0700

    Add more capability to programs

--- arbprec/tiny-bc/program.c
+++ arbprec/tiny-bc/program.c
@@ -1,34 +1,190 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "program.h"
 
-void bc_program_init(BcProgram* program) {
-	program->next = NULL;
-	program->num_stmts = 0;
-}
+static BcStatus bc_program_expand(BcProgram* program);
+static void bc_program_list_init(BcStmtList* list);
+static void bc_program_list_free(BcStmtList* list);
+static void bc_program_func_free(BcFunc* func);
+static void bc_program_var_free(BcVar* var);
+static void bc_program_array_free(BcArray* array);
 
-BcProgram* bc_program_expand(BcProgram* program) {
+BcStatus bc_program_init(BcProgram* program) {
 
-	// Malloc a new one.
-	BcProgram* next = malloc(sizeof(BcProgram));
+	// Check for invalid params.
+	if (program == NULL) {
+		return BC_STATUS_INVALID_PARAM;
+	}
+
+	// Malloc a new statement list.
+	program->first = malloc(sizeof(BcStmtList));
 
 	// Check for error.
-	if (next == NULL) {
-		return NULL;
+	if (program->first == NULL) {
+		return BC_STATUS_MALLOC_FAIL;
 	}
 
-	// Init the program.
-	bc_program_init(next);
+	// Initialize the list.
+	bc_program_list_init(program->first);
 
-	// If the parameter is not NULL, chain them.
-	if (program != NULL) {
-		program->next = next;
+	// Set the current list.
+	program->cur = program->first;
+
+	// Init symbol table.
+	program->num_funcs = 0;
+	program->num_vars = 0;
+	program->num_arrays = 0;
+
+	return BC_STATUS_SUCCESS;
+}
+
+BcStatus bc_program_insert(BcProgram* program, BcStmt* stmt) {
+
+	// Check for invalid parameters.
+	if (program == NULL || stmt == NULL) {
+		return BC_STATUS_INVALID_PARAM;
 	}
 
-	return next;
+	// If the list is full...
+	if (program->cur->num_stmts == BC_PROGRAM_MAX_STMTS) {
+
+		// Expand the list.
+		BcStatus status = bc_program_expand(program);
+
+		// Check for error.
+		if (status != BC_STATUS_SUCCESS) {
+			return status;
+		}
+	}
+
+	// Copy the data.
+	BcStmtList* cur = program->cur;
+	memcpy(cur->stmts + cur->num_stmts, stmt, sizeof(BcStmt));
+
+	return BC_STATUS_SUCCESS;
 }
 
 void bc_program_free(BcProgram* program) {
+
+	// Check for NULL.
+	if (program == NULL) {
+		return;
+	}
+
+	// These are used during the loop.
+	BcStmtList* temp;
+	BcStmtList* cur = program->first;
+
+	// Loop through the lists and free them all.
+	while (cur != NULL) {
+		temp = cur->next;
+		bc_program_list_free(cur);
+		cur = temp;
+	}
+
+	// Set these values to NULL.
+	program->cur = NULL;
+	program->first = NULL;
+
+	// Free funcs.
+	uint32_t num = program->num_funcs;
+	for (uint32_t i = 0; i < num; ++i) {
+		bc_program_func_free(program->funcs + i);
+	}
+
+	// Free vars.
+	num = program->num_vars;
+	for (uint32_t i = 0; i < num; ++i) {
+		bc_program_var_free(program->vars + i);
+	}
+
+	// Free arrays.
+	num = program->num_arrays;
+	for (uint32_t i = 0; i < num; ++i) {
+		bc_program_array_free(program->arrays + i);
+	}
+
+	// Zero these.
+	program->num_funcs = 0;
+	program->num_vars = 0;
+	program->num_arrays = 0;
+}
+
+static BcStatus bc_program_expand(BcProgram* program) {
+
+	// Check for NULL.
+	if (program == NULL) {
+		return BC_STATUS_INVALID_PARAM;
+	}
+
+	// Malloc a new one.
+	BcStmtList* next = malloc(sizeof(BcStmtList));
+
+	// Check for error.
+	if (next == NULL) {
+		return BC_STATUS_MALLOC_FAIL;
+	}
+
+	// Initialize the list.
+	bc_program_list_init(next);
+
+	// Set the pointers.
+	program->cur->next = next;
+	program->cur = next;
+
+	return BC_STATUS_SUCCESS;
+}
+
+static void bc_program_list_init(BcStmtList* list) {
+
+	// Check for NULL.
+	if (list == NULL) {
+		return;
+	}
+
+	// Zero these.
+	list->next = NULL;
+	list->num_stmts = 0;
+}
+
+static void bc_program_list_free(BcStmtList* list) {
+
+	// Check for NULL.
+	if (list == NULL) {
+		return;
+	}
+
+	// TODO: Write this function.
+}
+
+static void bc_program_func_free(BcFunc* func) {
+
+	// Check for NULL.
+	if (func == NULL) {
+		return;
+	}
+
+	// TODO: Write this function.
+}
+
+static void bc_program_var_free(BcVar* var) {
+
+	// Check for NULL.
+	if (var == NULL) {
+		return;
+	}
+
+	// TODO: Write this function.
+}
+
+static void bc_program_array_free(BcArray* array) {
+
+	// Check for NULL.
+	if (array == NULL) {
+		return;
+	}
+
 	// TODO: Write this function.
 }
--- arbprec/tiny-bc/program.h
+++ arbprec/tiny-bc/program.h
@@ -5,8 +5,21 @@
 
 #include "bc.h"
 
+/**
+ * @def BC_PROGRAM_MAX_STMTS
+ * The max number of statements in one statement list.
+ */
 #define BC_PROGRAM_MAX_STMTS (128)
 
+/**
+ * @def BC_PROGRAM_MAX_NAMES
+ * The max number of names that the program can have.
+ */
+#define BC_PROGRAM_MAX_NAMES (32767)
+
+/**
+ * The types that any statement can be.
+ */
 typedef enum BcStmtType {
 
 	BC_STMT_STRING,
@@ -22,7 +35,7 @@ typedef enum BcStmtType {
 	BC_STMT_WHILE,
 	BC_STMT_FOR,
 
-	BC_STMT_EXPR_VAR,
+	BC_STMT_EXPR_NAME,
 	BC_STMT_EXPR_ARRAY,
 	BC_STMT_EXPR_NUMBER,
 
@@ -47,28 +60,147 @@ typedef enum BcStmtType {
 	BC_STMT_EXPR_SCALE_FUNC,
 	BC_STMT_EXPR_IBASE,
 	BC_STMT_EXPR_OBASE,
+	BC_STMT_EXPR_LAST,
 	BC_STMT_EXPR_HALT,
 
 } BcStmtType;
 
+/**
+ * A union of data that statement types need.
+ */
+typedef union BcStmtData {
+
+	char* string;
+
+} BcStmtData;
+
+/**
+ * One statement in a program.
+ */
 typedef struct BcStmt {
 
 	BcStmtType type;
+	BcStmtData data;
 
 } BcStmt;
 
+/**
+ * A list of statements. Lists can be chained together.
+ */
+typedef struct BcStmtList {
+
+	/// The next list. NULL if none.
+	struct BcStmtList* next;
+
+	/// The number of statements that this list has.
+	uint32_t num_stmts;
+
+	/// The list of statements.
+	BcStmt stmts[BC_PROGRAM_MAX_STMTS];
+
+} BcStmtList;
+
+/**
+ * A function.
+ */
+typedef struct BcFunc {
+
+	/// The name of the function.
+	char* name;
+
+	/// The first of the statement lists.
+	BcStmtList* first;
+
+	/// The current statement list.
+	BcStmtList* cur;
+
+	/// The number of auto variables the function
+	/// has. This number includes parameters.
+	uint32_t num_autos;
+
+} BcFunc;
+
+/**
+ * A variable.
+ */
+typedef struct BcVar {
+
+	/// The name of the variable.
+	char* name;
+
+	/// The data. This will a pointer to the value
+	/// struct that will actually hold a value.
+	void* data;
+
+} BcVar;
+
+/**
+ * An array.
+ */
+typedef struct BcArray {
+
+	/// The name of the array.
+	char* name;
+
+	/// An array of data. This will be an array of
+	/// pointers to the structs that hold values.
+	void** array;
+
+} BcArray;
+
+/**
+ * A program. This holds all of the data
+ * that allows the program to be executed.
+ */
 typedef struct BcProgram {
 
-	struct BcProgram* next;
-	uint32_t num_stmts;
-	BcStmt stmts[BC_PROGRAM_MAX_STMTS];
+	/// The first of the statement lists.
+	BcStmtList* first;
+
+	/// The pointer to the current statement list.
+	BcStmtList* cur;
+
+	/// The number of functions in the program.
+	uint32_t num_funcs;
+
+	/// The list of functions.
+	BcFunc funcs[BC_PROGRAM_MAX_NAMES];
+
+	/// The number of variables in the program.
+	uint32_t num_vars;
+
+	/// The list of variables.
+	BcVar vars[BC_PROGRAM_MAX_NAMES];
+
+	/// The number of arrays in the program.
+	uint32_t num_arrays;
+
+	/// The list of arrays.
+	BcArray arrays[BC_PROGRAM_MAX_NAMES];
 
 } BcProgram;
 
-void bc_program_init(BcProgram* program);
+/**
+ * Initializes @a program.
+ * @param program	The program to initialize.
+ * @return			BC_STATUS_SUCCESS on success,
+ *					an error code otherwise.
+ */
+BcStatus bc_program_init(BcProgram* program);
 
-BcProgram* bc_program_expand(BcProgram* program);
+/**
+ * Inserts @a stmt into @a program.
+ * @param program	The program to insert into.
+ * @param stmt		The statement to insert.
+ * @return			BC_STATUS_SUCCESS on success,
+ *					an error code otherwise.
+ */
+BcStatus bc_program_insert(BcProgram* program, BcStmt* stmt);
 
+/**
+ * Frees @a program and all associated data.
+ * @param program	The program to free.
+ */
 void bc_program_free(BcProgram* program);
 
 #endif // BC_PROGRAM_H
