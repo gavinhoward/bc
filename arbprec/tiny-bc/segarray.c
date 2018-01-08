648794bfa46b2d587958bd6c3e94ed8b164abf9a
Gavin Howard <yzena.tech@gmail.com>
2018-01-08 13:34:58 -0700

    Finish writing function to add to a segmented array

--- arbprec/tiny-bc/segarray.c
+++ arbprec/tiny-bc/segarray.c
@@ -1,10 +1,13 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "segarray.h"
 
 static BcStatus bc_segarray_addArray(BcSegArray* sa);
-static BcStatus bc_segarray_insert(BcSegArray* sa, void* data);
+static inline void bc_segarray_moveLast(BcSegArray* sa, uint32_t end1);
+static void bc_segarray_move(BcSegArray* sa, uint32_t idx1, uint32_t num_elems);
 
 BcStatus bc_segarray_init(BcSegArray* sa, size_t esize, BcSegArrayCmpFunc cmp) {
 
@@ -41,9 +44,15 @@ BcStatus bc_segarrary_add(BcSegArray* sa, void* data) {
 		return BC_STATUS_INVALID_PARAM;
 	}
 
+	// Get the insert index and split it.
+	uint32_t idx = bc_segarray_find(sa, data);
+	uint32_t idx1 = BC_SEGARRAY_IDX1(idx);
+	uint32_t idx2 = BC_SEGARRAY_IDX2(idx);
+
 	// Get the indices of the end.
-	uint32_t end1 = BC_SEGARRAY_IDX1(sa->num);
-	uint32_t end2 = BC_SEGARRAY_IDX2(sa->num);
+	uint32_t end = sa->num;
+	uint32_t end1 = BC_SEGARRAY_IDX1(end);
+	uint32_t end2 = BC_SEGARRAY_IDX2(end);
 
 	// If we need to expand...
 	if (end2 == 0) {
@@ -55,17 +64,29 @@ BcStatus bc_segarrary_add(BcSegArray* sa, void* data) {
 		if (status != BC_STATUS_SUCCESS) {
 			return status;
 		}
+
+		// Move the last element of the previous array.
+		if ((end1 != 0 && end2 == 0) && idx != end) {
+			bc_segarray_moveLast(sa, end1);
+		}
 	}
 
-	// Get the insert index and split it.
-	uint32_t idx = bc_segarray_find(sa, data);
-	uint32_t idx1 = BC_SEGARRAY_IDX1(idx);
-	uint32_t idx2 = BC_SEGARRAY_IDX2(idx);
+	uint32_t last = BC_SEGARRAY_SEG_SIZE - 1;
 
-	uint32_t moveIdx = idx1;
-	while (moveIdx < end1) {
-
+	uint32_t move_idx = end2 == 0 ? end1 - 1 : end1;
+	while (move_idx > idx1) {
+		bc_segarray_move(sa, move_idx, last);
+		bc_segarray_moveLast(sa, move_idx);
+		--move_idx;
 	}
+
+	if (idx2 != last) {
+		bc_segarray_move(sa, idx1, last - idx2);
+	}
+
+	memcpy(bc_segarray_item2(sa, idx1, idx2), data, sa->esize);
+
+	return BC_STATUS_SUCCESS;
 }
 
 void* bc_segarray_item(BcSegArray* sa, uint32_t idx) {
@@ -81,7 +102,7 @@ void* bc_segarray_item2(BcSegArray* sa, uint32_t idx1, uint32_t idx2) {
 	}
 
 	// Get the pointer.
-	uint8_t* ptr = bc_stack_item(&sa->stack, idx1);
+	uint8_t* ptr = ((uint8_t**) sa->stack.stack)[idx1];
 
 	// Check for error.
 	if (ptr == NULL) {
@@ -158,6 +179,25 @@ static BcStatus bc_segarray_addArray(BcSegArray* sa) {
 	return bc_stack_push(&sa->stack, &ptr);
 }
 
-static BcStatus bc_segarray_insert(BcSegArray* sa, void* data) {
+static void bc_segarray_moveLast(BcSegArray* sa, uint32_t end1) {
+	uint8_t** ptr = (uint8_t**) sa->stack.stack;
+	uint8_t* dest = ptr[end1];
+	uint8_t* src = ptr[end1 - 1] + sa->esize * BC_SEGARRAY_SEG_SIZE;
+	memcpy(dest, src, sa->esize);
+}
 
+static void bc_segarray_move(BcSegArray* sa, uint32_t idx1, uint32_t num_elems) {
+
+	assert(num_elems < BC_SEGARRAY_SEG_SIZE);
+
+	uint8_t* ptr = ((uint8_t**) sa->stack.stack)[idx1];
+
+	size_t esize = sa->esize;
+
+	size_t move_size = esize * num_elems;
+
+	uint8_t* dest = ptr + esize * BC_SEGARRAY_SEG_SIZE - move_size;
+	uint8_t* src = dest - esize;
+
+	memmove(dest, src, move_size);
 }
