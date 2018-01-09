5a2555c8ce63b84f21736983b925b8a53718a242
Gavin Howard <yzena.tech@gmail.com>
2018-01-09 14:29:40 -0700

    Make sorting work

--- arbprec/tiny-bc/segarray.c
+++ arbprec/tiny-bc/segarray.c
@@ -9,6 +9,8 @@
 #include "program.h"
 
 static BcStatus bc_segarray_addArray(BcSegArray* sa, uint32_t idx);
+static inline void bc_segarray_moveLast(BcSegArray* sa, uint32_t end1);
+static void bc_segarray_move(BcSegArray* sa, uint32_t idx1, uint32_t start, uint32_t num_elems);
 
 BcStatus bc_segarray_init(BcSegArray* sa, size_t esize, BcSegArrayCmpFunc cmp) {
 
@@ -50,6 +52,12 @@ BcStatus bc_segarrary_add(BcSegArray* sa, void* data) {
 	uint32_t end1 = BC_SEGARRAY_IDX1(end);
 	uint32_t end2 = BC_SEGARRAY_IDX2(end);
 
+	// Get the insert index and split it.
+	uint32_t idx = bc_segarray_find(sa, data);
+	idx = idx == (uint32_t) -1 ? end : idx;
+	uint32_t idx1 = BC_SEGARRAY_IDX1(idx);
+	uint32_t idx2 = BC_SEGARRAY_IDX2(idx);
+
 	// If we need to expand...
 	if (end2 == 0 && end1 > 0) {
 
@@ -60,11 +68,29 @@ BcStatus bc_segarrary_add(BcSegArray* sa, void* data) {
 		if (status != BC_STATUS_SUCCESS) {
 			return status;
 		}
+
+		// Move the last element of the previous array.
+		if ((end1 != 0 && end2 == 0) && idx != end) {
+			bc_segarray_moveLast(sa, end1);
+		}
 	}
 
-	size_t esize = sa->esize;
-	uint8_t* ptr = sa->ptrs[end1];
-	memcpy(ptr + esize * end2, data, esize);
+	if (end1 > 0) {
+
+		uint32_t move_idx = end2 == 0 ? end1 - 1 : end1;
+		while (move_idx > idx1) {
+			bc_segarray_move(sa, move_idx, 0, BC_SEGARRAY_SEG_LAST);
+			bc_segarray_moveLast(sa, move_idx);
+			--move_idx;
+		}
+	}
+
+	if (idx2 != BC_SEGARRAY_SEG_LAST) {
+		bc_segarray_move(sa, idx1, idx2, BC_SEGARRAY_SEG_LAST - idx2);
+	}
+
+	uint8_t* ptr = sa->ptrs[idx1];
+	memcpy(ptr + sa->esize * idx2, data, sa->esize);
 
 	++sa->num;
 
@@ -99,20 +125,32 @@ void* bc_segarray_item2(BcSegArray* sa, uint32_t idx1, uint32_t idx2) {
 
 uint32_t bc_segarray_find(BcSegArray* sa, void* data) {
 
+	// Cache this.
 	BcSegArrayCmpFunc cmp = sa->cmp;
-	uint32_t num = sa->num;
 
-	for (uint32_t i = 0; i < num; ++i) {
+	// Set the high and low.
+	uint32_t low = 0;
+	uint32_t high = sa->num;
 
-		uint32_t idx1 = BC_SEGARRAY_IDX1(i);
-		uint32_t idx2 = BC_SEGARRAY_IDX2(i);
+	// Loop until the index is found.
+	while (low < high) {
 
-		if (!cmp(bc_segarray_item2(sa, idx1, idx2), data)) {
-			return i;
+		// Calculate the mid point.
+		uint32_t mid = (low + high) / 2;
+
+		// Get the pointer.
+		uint8_t* ptr = bc_segarray_item(sa, mid);
+
+		// Figure out what to do.
+		if (cmp(ptr, data) > 0) {
+			high = mid;
+		}
+		else {
+			low = mid + 1;
 		}
 	}
 
-	return (uint32_t) -1;
+	return low;
 }
 
 void bc_segarray_free(BcSegArray* sa) {
@@ -151,4 +189,27 @@ static BcStatus bc_segarray_addArray(BcSegArray* sa, uint32_t idx) {
 	sa->ptrs[idx] = ptr;
 
 	return BC_STATUS_SUCCESS;
+}
+
+static void bc_segarray_moveLast(BcSegArray* sa, uint32_t end1) {
+	uint8_t* dest = sa->ptrs[end1];
+	uint8_t* src = sa->ptrs[end1 - 1] + sa->esize * BC_SEGARRAY_SEG_LAST;
+	memcpy(dest, src, sa->esize);
+}
+
+static void bc_segarray_move(BcSegArray* sa, uint32_t idx1, uint32_t start, uint32_t num_elems) {
+
+	assert(num_elems < BC_SEGARRAY_SEG_SIZE &&
+	       start + num_elems < BC_SEGARRAY_SEG_SIZE);
+
+	uint8_t* ptr = sa->ptrs[idx1];
+
+	size_t esize = sa->esize;
+
+	size_t move_size = esize * num_elems;
+
+	uint8_t* src = ptr + esize * start;
+	uint8_t* dest = src + esize;
+
+	memmove(dest, src, move_size);
 }
