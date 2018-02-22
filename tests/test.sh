8e4ffd7ca465130f669cb19ebd927a37a60e3654
Gavin Howard <yzena.tech@gmail.com>
2018-02-22 11:22:21 -0700

    Make test.sh better to use

--- tests/test.sh
+++ tests/test.sh
@@ -1,33 +1,49 @@
 #! /bin/sh
 
-if [ "$#" -lt 4 ]; then
-	echo "usage: test.sh <test> <bc> <test_output1> <test_output2>"
-	exit 1
-fi
-
 script="$0"
 
 testdir=$(dirname "$script")
 
+if [ "$#" -lt 1 ]; then
+	echo "usage: test.sh test [bc test_output1 test_output2]"
+	echo "valid tests are:"
+	echo ""
+	cat "$testdir/all.txt"
+	echo ""
+	exit 1
+fi
+
 set -e
 
-name="$testdir/$1"
+name="$testdir/$1.txt"
 shift
 
-bc="$1"
-shift
+if [ "$#" -gt 0 ]; then
+	bc="$1"
+	shift
+else
+	bc="$testdir/../bc"
+fi
 
-out1="$1"
-shift
+if [ "$#" -gt 0 ]; then
+	out1="$1"
+	shift
+else
+	out1="$testdir/../log_bc.txt"
+fi
 
-out2="$1"
-shift
+if [ "$#" -gt 0 ]; then
+	out2="$1"
+	shift
+else
+	out2="$testdir/../log_test.txt"
+fi
 
 rm -rf "$out1"
 rm -rf "$out2"
 
-cat "$name.txt" | bc -lq >> "$out1"
+cat "$name" | bc -lq >> "$out1"
 
-cat "$name.txt" | "$bc" -lq >> "$out2"
+cat "$name" | "$bc" -lq >> "$out2"
 
 diff "$out1" "$out2"
