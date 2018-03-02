86c9b7909838fee96dd5f288249efc6c80609193
Gavin Howard <yzena.tech@gmail.com>
2018-03-02 11:34:32 -0700

    Style fix for toybox

--- include/num.h
+++ include/num.h
@@ -75,7 +75,7 @@ BcStatus bc_num_copy(void *dest, void *src);
 BcStatus bc_num_parse(BcNum *n, const char *val, BcNum *base, size_t base_t);
 
 BcStatus bc_num_print(BcNum *n, BcNum *base, size_t base_t);
-BcStatus bc_num_fprint(BcNum *n, BcNum *base, size_t base_t, FILE* f);
+BcStatus bc_num_fprint(BcNum *n, BcNum *base, size_t base_t, FILE *f);
 
 BcStatus bc_num_long(BcNum *n, long *result);
 BcStatus bc_num_ulong(BcNum *n, unsigned long *result);
