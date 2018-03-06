206efbdd0512dbcda6f68feeb50ba0bc61d8e865
Gavin Howard <yzena.tech@gmail.com>
2018-03-05 20:23:02 -0700

    Add an initial try at a timeconst.bc tester

---
+++ tests/timeconst.sh
@@ -0,0 +1,58 @@
+#! /bin/sh
+
+# Tests the timeconst.bc script from the Linux kernel build.
+# You can find the script at kernel/time/timeconst.bc in any Linux repo.
+# One such repo is: https://github.com/torvalds/linux
+
+script="$0"
+
+testdir=$(dirname "$script")
+
+if [ "$#" -gt 4 ]; then
+	echo "usage: $0 [timeconst.bc bc test_output1 test_output2]"
+	exit 1
+fi
+
+set -e
+
+if [ "$#" -gt 0 ]; then
+	timeconst="$1"
+	shift
+else
+	timeconst="$testdir/../../timeconst.bc"
+fi
+
+if [ "$#" -gt 0 ]; then
+	bc="$1"
+	shift
+else
+	bc="$testdir/../bc"
+fi
+
+if [ "$#" -gt 0 ]; then
+	out1="$1"
+	shift
+else
+	out1="$testdir/../log_bc.txt"
+fi
+
+if [ "$#" -gt 0 ]; then
+	out2="$1"
+	shift
+else
+	out2="$testdir/../log_test.txt"
+fi
+
+rm -rf "$out1"
+rm -rf "$out2"
+
+for i in {1..10000}; do
+
+	echo "$i"
+
+	(echo $i | bc -q "$timeconst" ) > "$out1"
+	(echo $i | "$bc" -q "$timeconst" ) > "$out2"
+
+	diff "$out1" "$out2"
+
+done
