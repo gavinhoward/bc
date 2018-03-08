91d2f61d67fe6f5e77ce05d59e7080b2ca19dd5b
Gavin Howard <yzena.tech@gmail.com>
2018-03-08 11:52:45 -0700

    Modify gen to generate a full header

--- src/lib/gen.c
+++ src/lib/gen.c
@@ -26,30 +26,33 @@
 
 #include <libgen.h>
 
-static const char *bc_gen_usage = "usage: gen bc_script output\n";
-static const char *bc_gen_header =
-  "/*\n * *** AUTOMATICALLY GENERATED from %s. ***\n"
-  " * *** DO NOT MODIFY ***\n */\n\n";
+static const char *bc_gen_usage = "usage: gen bc_script output [header]\n";
 
 #define INVALID_PARAMS (1)
 #define MALLOC_FAIL (2)
 #define INVALID_INPUT_FILE (3)
 #define INVALID_OUTPUT_FILE (4)
-#define IO_ERR (5)
+#define INVALID_HEADER_FILE (5)
+#define IO_ERR (6)
 
 int main(int argc, char *argv[]) {
 
-  FILE* in;
-  FILE* out;
+  FILE *in;
+  FILE *out;
+  FILE *header;
+  char *header_name;
+  char *header_buf;
   int c;
   int count;
+  char *buffer;
+  size_t len, read;
   char *buf;
   char *base;
   int err;
 
   err = 0;
 
-  if (argc != 3) {
+  if (argc < 3) {
     printf("%s", bc_gen_usage);
     return INVALID_PARAMS;
   }
@@ -76,9 +79,51 @@ int main(int argc, char *argv[]) {
     goto out_err;
   }
 
+  if (argc >= 4) header_name = argv[3];
+  else header_name = "src/lib/header.c";
+
+  header = fopen(header_name, "r");
+
+  if (!header) {
+    err = INVALID_HEADER_FILE;
+    goto header_err;
+  }
+
+  fseek(header, 0, SEEK_END);
+  len = ftell(header);
+  fseek(header, 0, SEEK_SET);
+
+  buffer = malloc(len + 1);
+
+  if (!buffer) {
+    err = MALLOC_FAIL;
+    goto buffer_err;
+  }
+
+  read = fread(buffer, 1, len, header);
+
+  if (read != len) {
+    err = IO_ERR;
+    goto header_buf_err;
+  }
+
+  buffer[len] = '\0';
+
+  header_buf = malloc(len + strlen(base) + 1);
+
+  if (!header_buf) {
+    err= MALLOC_FAIL;
+    goto header_buf_err;
+  }
+
+  if (sprintf(header_buf, buffer, base) < 0) {
+    err = IO_ERR;
+    goto error;
+  }
+
   count = 0;
 
-  if (fprintf(out, bc_gen_header, base) < 0) {
+  if (fputs(header_buf, out) == EOF) {
     err = IO_ERR;
     goto error;
   }
@@ -137,6 +182,18 @@ int main(int argc, char *argv[]) {
   }
 
 error:
+
+  free(header_buf);
+
+header_buf_err:
+
+  free(buffer);
+
+buffer_err:
+
+  fclose(header);
+
+header_err:
 
   fclose(out);
 
