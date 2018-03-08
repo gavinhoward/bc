7f6911ca0c47393a402d85684c02a3d74c90bfd5
Gavin Howard <yzena.tech@gmail.com>
2018-03-08 12:14:27 -0700

    Add files to cut releases for toybox

---
+++ toybox/header.c
@@ -0,0 +1,49 @@
+/* bc.c - An implementation of POSIX bc.
+ *
+ * Copyright 2018 Gavin D. Howard <yzena.tech@gmail.com>
+ *
+ * See http://pubs.opengroup.org/onlinepubs/9699919799/utilities/bc.html
+
+USE_BC(NEWTOY(bc, "cilqsw", TOYFLAG_USR|TOYFLAG_BIN|TOYFLAG_LOCALE))
+
+config BC
+  bool "bc"
+  default n
+  help
+    usage: bc [-cilqsw] [long-options] [file ...]
+
+    bc is a command-line calculator with a Turing-complete language.
+
+    options:
+
+      -c  print generated code (for debugging)
+      -i  force interactive mode
+      -l  use predefined math routines:
+
+          s(expr)  =  sine of expr in radians
+          c(expr)  =  cosine of expr in radians
+          a(expr)  =  arctangent of expr, returning radians
+          l(expr)  =  natural log of expr
+          e(expr)  =  raises e to the power of expr
+          j(n, x)  =  Bessel function of integer order n of x
+
+      -q  don't print version and copyright
+      -s  error if any non-POSIX extensions are used
+      -w  warn if any non-POSIX extensions are used
+
+*/
+
+#include <stdbool.h>
+
+#define FOR_bc
+#include "toys.h"
+
+GLOBALS(
+    long bc_code;
+    long bc_interactive;
+    long bc_std;
+    long bc_warn;
+
+    long bc_signal;
+)
+
---
+++ toybox/release.py
@@ -0,0 +1,61 @@
+#! /usr/bin/python3 -B
+
+import sys
+import os
+import subprocess
+import regex as re
+
+cwd = os.path.realpath(os.getcwd())
+
+script = os.path.realpath(sys.argv[0])
+
+testdir = os.path.dirname(script)
+
+if len(sys.argv) < 2:
+	print("usage: {} toybox_dir".format(script))
+	sys.exit(1)
+
+toybox = sys.argv[1].rstrip(os.sep)
+
+toybox_bc = toybox + "/toys/pending/bc.c"
+
+os.chdir(testdir)
+os.chdir("..")
+
+res = subprocess.run(["make", "bc_mathlib"])
+
+if res.returncode != 0:
+	sys.exit(res.returncode)
+
+content = ""
+
+with open(testdir + "/files.txt") as f:
+	files = f.read().splitlines()
+
+for name in files:
+
+	with open(name) as f:
+		lines = f.readlines()
+
+	# Skip the header lines.
+	for i in range(22, len(lines)):
+		content += lines[i]
+
+regexes = [ '^#include .*$', '^#ifndef BC.*_H$', '^#define BC.*_H$', '^#endif \/\/ BC.*_H$', '^extern.*$' ]
+
+for reg in regexes:
+	r = re.compile(reg, re.M)
+	content = r.sub('', content)
+
+with open(testdir + "/header.c") as f:
+	content = f.read() + content
+
+with open(testdir + "/footer.c") as f:
+	content += f.read()
+
+content = re.sub('\n\n\n+', '\n\n', content)
+
+os.chdir(cwd)
+
+with open(toybox_bc, 'w') as f:
+	f.write(content)
