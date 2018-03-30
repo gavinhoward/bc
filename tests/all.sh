9269c2d33617b81562b1d955fabc32e61cede3d9
Gavin Howard <yzena.tech@gmail.com>
2018-03-30 11:40:59 -0600

    Make all.sh and scripts.sh use sh, not bash

--- tests/all.sh
+++ tests/all.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 
 set -e
 
@@ -17,12 +17,12 @@ bcdir=$(dirname "${bc}")
 out1="$bcdir/log_bc.txt"
 out2="$bcdir/log_test.txt"
 
-while read -u 10 t; do
+while read t; do
 
 	echo "$t"
 	"$testdir/test.sh" "$t" "$bc" "$out1" "$out2"
 
-done 10< "$testdir/all.txt"
+done < "$testdir/all.txt"
 
 "$testdir/scripts.sh" "$bc" "$out1" "$out2"
 
