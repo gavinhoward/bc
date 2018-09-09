08d3940344aeaabdad4ca475c5cf77ca6cb3bff2
Gavin Howard <yzena.tech@gmail.com>
2018-09-08 22:55:43 -0600

    Redo error tests into one file

--- tests/errors.sh
+++ tests/errors.sh
@@ -5,7 +5,7 @@
 script="$0"
 
 testdir=$(dirname "$script")
-errdir="$testdir/errors"
+testfile="$testdir/errors.txt"
 
 if [ "$#" -lt 1 ]; then
 	bc="$testdir/../bc"
@@ -13,28 +13,17 @@ else
 	bc="$1"
 fi
 
-for i in $(seq 4 34); do
+while read -r line; do
 
-	testfile="$errdir/$i.txt"
+	echo "Test: $line"
 
-	echo "Running tests for error code $i"
+	echo "$line" | "$bc" "$@" -l 2>/dev/null
+	error="$?"
 
-	while read -r line; do
+	if [ "$error" -eq 0 ]; then
+		echo "\nbc returned no error"
+		echo "exiting..."
+		exit 127
+	fi
 
-		echo "    Test: $line"
-
-		echo "$line" | "$bc" "$@" -l 2>/dev/null
-		error="$?"
-
-		if [ "$error" -ne "$i" ]; then
-			echo "\nbc returned wrong error code: $error"
-			echo "    should have been $i"
-			echo "exiting..."
-			exit 127
-		fi
-
-	done < "$testfile"
-
-	echo ""
-
-done
+done < "$testfile"
