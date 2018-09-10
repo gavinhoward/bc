f79c752aeb9c4fb75e5dd9d8e5bd7a337f9714d5
Gavin Howard <yzena.tech@gmail.com>
2018-09-10 11:50:52 -0600

    Add Posix error tests

--- tests/errors.sh
+++ tests/errors.sh
@@ -13,19 +13,34 @@ else
 	bc="$1"
 fi
 
-echo "Running errors..."
+errors="$testdir/errors.txt"
+posix_errors="$testdir/posix_errors.txt"
 
-while read -r line; do
+for testfile in "$errors" "$posix_errors"; do
 
-	echo "    Test: $line"
-
-	echo "$line" | "$bc" "$@" -l 2>/dev/null
-	error="$?"
-
-	if [ "$error" -eq 0 ]; then
-		echo "\nbc returned no error"
-		echo "exiting..."
-		exit 127
+	if [ "$testfile" = "$posix_errors" ]; then
+		options="-ls"
+	else
+		options="-l"
 	fi
 
-done < "$testfile"
+	base=$(basename "$testfile")
+	base="${base%.*}"
+	echo "\nRunning $base..."
+
+	while read -r line; do
+
+		echo "    Test: $line"
+
+		echo "$line" | "$bc" "$@" "$options" 2>/dev/null
+		error="$?"
+
+		if [ "$error" -eq 0 ]; then
+			echo "\nbc returned no error"
+			echo "exiting..."
+			exit 127
+		fi
+
+	done < "$testfile"
+
+done
