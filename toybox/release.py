22a0832cdc6c82149e04bd52ddc102dfab6f6702
Gavin Howard <yzena.tech@gmail.com>
2018-09-12 11:00:53 -0600

    Update toybox files for lib.bc move

--- toybox/release.py
+++ toybox/release.py
@@ -40,7 +40,7 @@ toybox_bc = toybox + "/toys/pending/bc.c"
 os.chdir(testdir)
 os.chdir("..")
 
-res = subprocess.run(["make", "lib/lib.c"])
+res = subprocess.run(["make", "gen/lib.c"])
 
 if res.returncode != 0:
 	sys.exit(res.returncode)
