022606d7fd65f5dafa8e142a17339fa18b993fed
Gavin Howard <yzena.tech@gmail.com>
2018-10-08 08:51:26 -0600

    Rename some local vars for an upcoming change

--- src/lang.c
+++ src/lang.c
@@ -99,22 +99,22 @@ err:
 
 BcStatus bc_array_copy(BcVec *d, const BcVec *s) {
 
-	BcStatus status;
+	BcStatus st;
 	size_t i;
 
 	assert(d && s && d != s && d->size == s->size && d->dtor == s->dtor);
 
 	bc_vec_npop(d, d->len);
-	if ((status = bc_vec_expand(d, s->cap))) return status;
+	if ((st = bc_vec_expand(d, s->cap))) return st;
 	d->len = s->len;
 
-	for (i = 0; !status && i < s->len; ++i) {
+	for (i = 0; !st && i < s->len; ++i) {
 		BcNum *dnum = bc_vec_item(d, i), *snum = bc_vec_item(s, i);
-		if ((status = bc_num_init(dnum, snum->len))) return status;
-		if ((status = bc_num_copy(dnum, snum))) bc_num_free(dnum);
+		if ((st = bc_num_init(dnum, snum->len))) return st;
+		if ((st = bc_num_copy(dnum, snum))) bc_num_free(dnum);
 	}
 
-	return status;
+	return st;
 }
 
 BcStatus bc_array_expand(BcVec *a, size_t len) {
