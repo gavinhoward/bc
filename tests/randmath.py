fd06ac284c549bbc972b43e46356ff826dde35b4
Gavin Howard <yzena.tech@gmail.com>
2018-10-10 16:48:32 -0600

    Change randmath.sh to Python

---
+++ tests/randmath.py
@@ -0,0 +1,301 @@
+#! /usr/bin/python3 -B
+#
+# Copyright 2018 Gavin D. Howard
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+#
+
+import os, errno
+import random
+import sys
+import subprocess
+
+def silentremove(filename):
+	try:
+		os.remove(filename)
+	except OSError as e:
+		if e.errno != errno.ENOENT:
+			raise
+
+def finish():
+	silentremove(out1)
+	silentremove(out1)
+	silentremove(math)
+	silentremove(results)
+	silentremove(opfile)
+
+def gen(limit=4):
+	return random.randint(0, 2 ** (8 * limit))
+
+def negative():
+	return random.randint(0, 1) == 1
+
+def zero():
+	return random.randint(0, 2 ** (8) - 1) == 0
+
+def num(neg, real, z, limit=4):
+
+	if z:
+		z = zero()
+	else:
+		z = False
+
+	if z:
+		return 0
+
+	if neg:
+		neg = negative()
+
+	g = gen(limit)
+
+	if real and negative():
+		n = gen(25)
+	else:
+		n = 0
+
+	g = str(g)
+	if n != 0:
+		g = g + "." + str(n)
+
+	if neg and g != "0":
+		g = "-" + g
+
+	return g
+
+
+def add(test, expected, op):
+
+	with open(math, "a") as f:
+		f.write(test + "\n")
+
+	with open(results, "a") as f:
+		f.write(expected + "\n")
+
+	with open(opfile, "a") as f:
+		f.write(str(op) + "\n")
+
+def compare(exe, options, p, test, halt, expected, op, do_add=True):
+
+	if p.returncode != 0:
+
+		print("    {} returned an error ({})".format(exe, p.returncode))
+
+		if do_add:
+			print("    adding {} to checklist...".format(test))
+			add(test, expected, op)
+
+		return
+
+	actual = p.stdout.decode()
+
+	if actual != expected:
+
+		if op >= exponent:
+
+			indata = "scale += 10; {}; {}".format(test, halt)
+			args = [ exe, options ]
+			p2 = subprocess.run(args, input=indata.encode(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			expected = p2.stdout[:-10].decode()
+
+			if actual == expected:
+				print("    failed because of bug in other {}".format(exe))
+				print("    continuing...")
+				return
+
+		print("   failed \"{}\"".format(test))
+		print("    expected:")
+		print("        {}".format(expected))
+		print("    actual:")
+		print("        {}".format(actual))
+
+		if do_add:
+			print("   adding to checklist...")
+			add(test, expected, op)
+
+
+def gen_test(op):
+
+	scale = num(False, False, True, 5 / 8)
+
+	if op < div:
+		s = fmts[op].format(scale, num(True, True, True), num(True, True, True))
+	elif op == div or op == mod:
+		s = fmts[op].format(scale, num(True, True, True), num(True, True, False))
+	elif op == power:
+		s = fmts[op].format(scale, num(True, True, True, 7 / 8), num(True, False, True, 6 / 8))
+	elif op == modexp:
+		s = fmts[op].format(scale, num(True, False, True), num(True, False, True),
+		                    num(True, False, False))
+	elif op == sqrt:
+		s = "1"
+		while s == "1":
+			s = num(False, True, True, 1)
+		s = fmts[op].format(scale, s)
+	else:
+
+		if op == exponent:
+			first = num(True, True, True, 6 / 8)
+		elif op == bessel:
+			first = num(False, True, True, 6 / 8)
+		else:
+			first = num(True, True, True)
+
+		if op != bessel:
+			s = fmts[op].format(scale, first)
+		else:
+			s = fmts[op].format(scale, first, 6 / 8)
+
+	return s
+
+def run_test(t):
+
+	op = random.randrange(bessel + 1)
+
+	if op != modexp:
+		exe = "bc"
+		halt = "halt"
+		options = "-lq"
+	else:
+		exe = "dc"
+		halt = "q"
+		options = ""
+
+	test = gen_test(op)
+
+	bcexe = exedir + "/" + exe
+	indata = test + "\n" + halt
+
+	args = [ exe, options ]
+
+	print("Test {}: {}".format(t, test))
+
+	p = subprocess.run(args, input=indata.encode(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+	if p.returncode != 0 or p.stdout.decode() == "":
+		print("    other {} returned an error ({}); continuing...".format(exe, p.returncode))
+		return
+
+	output1 = p.stdout.decode()
+
+	args = [ bcexe, options ]
+
+	p = subprocess.run(args, input=indata.encode(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	compare(exe, options, p, test, halt, output1, op)
+
+
+if __name__ != "__main__":
+	sys.exit(1)
+
+script = sys.argv[0]
+testdir = os.path.dirname(script)
+
+exedir = testdir + "/.."
+out1 = exedir + "/.log_bc.txt"
+out2 = exedir + "/.log_test.txt"
+math = exedir + "/.math.txt"
+results = exedir + "/.results.txt"
+opfile = exedir + "/.ops.txt"
+
+ops = [ '+', '-', '*', '/', '%', '^', '|' ]
+files = [ "add", "subtract", "multiply", "divide", "modulus", "power", "modexp",
+          "sqrt", "exponent", "log", "arctangent", "sine", "cosine", "bessel" ]
+funcs = [ "sqrt", "e", "l", "a", "s", "c", "j" ]
+
+fmts = [ "scale = {}; {} + {}", "scale = {}; {} - {}", "scale = {}; {} * {}",
+         "scale = {}; {} / {}", "scale = {}; {} % {}", "scale = {}; {} ^ {}",
+         "{}k {} {} {}|pR", "scale = {}; sqrt({})", "scale = {}; e({})",
+         "scale = {}; l({})", "scale = {}; a({})", "scale = {}; s({})",
+         "scale = {}; c({})", "scale = {}; j({}, {})" ]
+
+div = 3
+mod = 4
+power = 5
+modexp = 6
+sqrt = 7
+exponent = 8
+bessel = 13
+
+finish()
+
+try:
+	i = 0
+	while True:
+		run_test(i)
+		i = i + 1
+except KeyboardInterrupt:
+	pass
+
+if not os.path.exists(math):
+	print("\nNo items in checklist.")
+	print("Exiting")
+	sys.exit(0)
+
+print("\nGoing through the checklist...\n")
+
+with open(math, "r") as f:
+	tests = f.readlines()
+
+with open(results, "r") as f:
+	expecteds = f.readlines()
+
+with open(opfile, "r") as f:
+	ops = f.readlines()
+
+if len(tests) != len(expecteds) and len(tests) != len(ops):
+	print("Corrupted checklist!")
+	print("Exiting...")
+	sys.exit(1)
+
+for i in range(0, len(tests)):
+
+	#print("Test: {}\nExpected: {}\nOp: {}".format(tests[i], expecteds[i], ops[i]))
+	#continue
+
+	print("\n{}".format(tests[i]))
+
+	if int(ops[i]) != modexp:
+		exe = "bc"
+		halt = "halt"
+		options = "-lq"
+	else:
+		exe = "dc"
+		halt = "q"
+		options = ""
+
+	bcexe = exedir + "/" + exe
+
+	args = [ bcexe, options ]
+	indata = tests[i] + "\n" + halt
+
+	p = subprocess.run(args, input=indata.encode(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+	compare(exe, options, p, tests[i], halt, expecteds[i], ops[i], False)
+
+	answer = input("\nAdd test to test suite? [y/N]: ")
+
+	if 'Y' in answer or 'y' in answer:
+		print("Yes")
+		continue
+
+		name = testdir + "/" + exe + "/" + files[ops[i]]
+
+		with open(name + ".txt", "a") as f:
+			f.write(tests[i])
+
+		with open(name + "_results.txt", "a") as f:
+			f.write(expecteds[i])
+
+	else:
+		print("No")
+
+
+finish()
