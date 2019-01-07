b7ffaa87ae652fe2c85d9a9fd355f1de30eeb275
Gavin Howard <yzena.tech@gmail.com>
2019-01-07 12:47:27 -0700

    Up a limit

--- include/vm.h
+++ include/vm.h
@@ -76,7 +76,7 @@
 #define BC_MAX_STRING ((unsigned long) UINT_MAX - 1)
 #define BC_MAX_NAME BC_MAX_STRING
 #define BC_MAX_NUM BC_MAX_STRING
-#define BC_MAX_EXP ((unsigned long) LONG_MAX)
+#define BC_MAX_EXP ((unsigned long) ULONG_MAX)
 #define BC_MAX_VARS ((unsigned long) SIZE_MAX - 1)
 
 #define BC_IS_BC (BC_ENABLED && (!DC_ENABLED || vm->name[0] != 'd'))
