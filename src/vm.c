a19866f29707ce1fe953d03b857eb67fdfa2aabd
Gavin Howard <yzena.tech@gmail.com>
2019-02-25 13:48:22 -0700

    Shorten some code

--- src/vm.c
+++ src/vm.c
@@ -229,16 +229,15 @@ static void bc_vm_exit(BcError e) {
 }
 
 size_t bc_vm_arraySize(size_t n, size_t size) {
-	size_t result = n * size;
-	if (BC_ERR(n != 0 && result / n != size))
-		bc_vm_exit(BC_ERROR_FATAL_ALLOC_ERR);
-	return result;
+	size_t res = n * size;
+	if (BC_ERR(n != 0 && res / n != size)) bc_vm_exit(BC_ERROR_FATAL_ALLOC_ERR);
+	return res;
 }
 
 size_t bc_vm_growSize(size_t a, size_t b) {
-	size_t result = a + b;
-	if (BC_ERR(result < a || result < b)) bc_vm_exit(BC_ERROR_FATAL_ALLOC_ERR);
-	return result;
+	size_t res = a + b;
+	if (BC_ERR(res < a || res < b)) bc_vm_exit(BC_ERROR_FATAL_ALLOC_ERR);
+	return res;
 }
 
 void* bc_vm_malloc(size_t n) {
