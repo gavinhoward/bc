04f4fccef579b7e931b59e09028277101704ca3a
Gavin Howard <yzena.tech@gmail.com>
2019-02-26 16:41:15 -0700

    Tie BC_NUM_KARATSUBA_LEN to BC_NUM_DEF_SIZE
    This is to prevent realloc errors in bc_num_k().

--- include/num.h
+++ include/num.h
@@ -52,7 +52,7 @@ typedef struct BcNum {
 
 #ifndef BC_NUM_KARATSUBA_LEN
 #define BC_NUM_KARATSUBA_LEN (64)
-#elif BC_NUM_KARATSUBA_LEN < 16
+#elif BC_NUM_KARATSUBA_LEN < BC_NUM_DEF_SIZE
 #error BC_NUM_KARATSUBA_LEN must be at least 16
 #endif // BC_NUM_KARATSUBA_LEN
 
