0ef1e401eeede897c0fc000d40e7b32316507453
Gavin Howard <yzena.tech@gmail.com>
2019-02-27 19:29:02 -0700

    Fix another crash found by afl

--- src/dc/parse.c
+++ src/dc/parse.c
@@ -38,7 +38,7 @@ static BcStatus dc_parse_register(BcParse *p) {
 
 	s = bc_lex_next(&p->l);
 	if (BC_ERR(s)) return s;
-	assert(p->l.t == BC_LEX_NAME);
+	if (p->l.t != BC_LEX_NAME) return bc_parse_err(p, BC_ERROR_PARSE_TOKEN);
 
 	bc_parse_pushName(p, p->l.str.v);
 
