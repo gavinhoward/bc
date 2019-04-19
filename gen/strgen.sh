7d9fb3e6ffb7008afec4c7168498751366213b0f
Stefan Esser <se@freebsd.org>
2019-04-19 16:09:03 +0200

    Provide shell version of strgen.c to allow use without compilation.
    The output has been verified to be identical to that of the C language
    version for all files in this project by comparing the resulting .o files.

---
+++ gen/strgen.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+export LANG=C
+export LC_CTYPE=C
+
+progname=${0##*/}
+
+if [ $# -lt 4 ]; then
+	echo "usage: $progname input output name header [label [define [remove_tabs]]]"
+	exit 1
+fi
+
+input="$1"
+output="$2"
+name="$3"
+header="$4"
+label="$5"
+define="$6"
+remove_tabs="$7"
+
+exec < "$input"
+exec > "$output"
+
+if [ -n "$label" ]; then
+	nameline="const char *${label} = \"${input}\";"
+fi
+
+if [ -n "$define" ]; then
+	condstart="#if ${define}"
+	condend="#endif"
+fi
+
+if [ -n "$remove_tabs" -a "$remove_tabs" -ne 0 ]; then
+	remtabsexpr='s:	::g;'
+fi
+
+cat<<EOF
+// Licensed under the 2-clause BSD license.
+// *** AUTOMATICALLY GENERATED FROM ${input}. DO NOT MODIFY. ***
+
+${condstart}
+#include <${header}>
+
+$nameline
+
+const char ${name}[] =
+$(sed -e "$remtabsexpr "'1,/^$/d; s:\\n:\\\\n:g; s:":\\":g; s:^:":; s:$:\\n":')
+;
+${condend}
+EOF
+
+
+exit 0
+
+#if BC_ENABLED && BC_ENABLE_EXTRA_MATH
+#include <bc.h>
+
+const char *bc_lib2_name = "gen/lib2.bc";
+
+const char bc_lib2[] = {
