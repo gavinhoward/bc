3ade5329ba581dc7dea7921d4b7e4baf07313265
Gavin Howard <yzena.tech@gmail.com>
2019-06-19 22:16:02 -0600

    Fix style

--- include/num.h
+++ include/num.h
@@ -186,7 +186,7 @@ BcStatus bc_num_divmod(BcNum *a, BcNum *b, BcNum *c, BcNum *d, size_t scale);
 size_t bc_num_addReq(const BcNum* a, const BcNum* b, size_t scale);
 
 size_t bc_num_mulReq(const BcNum *a, const BcNum *b, size_t scale);
-size_t bc_num_powReq(const BcNum* a, const BcNum* b, size_t scale);
+size_t bc_num_powReq(const BcNum *a, const BcNum *b, size_t scale);
 #if BC_ENABLE_EXTRA_MATH
 size_t bc_num_placesReq(const BcNum *a, const BcNum *b, size_t scale);
 #endif // BC_ENABLE_EXTRA_MATH
