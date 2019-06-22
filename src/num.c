e212d54c68945b2ed8b229b0c11c2124c62b5e92
Gavin Howard <yzena.tech@gmail.com>
2019-06-21 19:25:40 -0600

    Fix style

--- src/num.c
+++ src/num.c
@@ -1420,6 +1420,7 @@ static BcBigDig bc_num_parseChar(char c, size_t base_t) {
 
 	return (BcBigDig) (uchar) c;
 }
+
 static void bc_num_parseDecimal(BcNum *restrict n, const char *restrict val) {
 
 	size_t len, i, temp, mod;
