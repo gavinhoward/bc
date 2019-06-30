gavinhoward/bc

commit 2b00094faf54d8e1a8009b275565f34ea95fe5fc

Fix and add a test for a bug
This bug is that if a var needed to be pushed, and it wasn't last, the
var was pulled before the type check happened. This caused an assert to
(rightly) fire. The type check was moved before, and it all went away.

As said in the tag line, this commit also adds the file generated by afl
that triggered the bug.

src/program.c
@@ -725,21 +725,19 @@ static BcStatus bc_program_copyToVar(BcProgram *p, size_t idx,
 	bool var = (t == BC_TYPE_VAR);
 
 #if BC_ENABLED
-	if (last) s = bc_program_operand(p, &ptr, &n, 0);
-	else {
-		ptr = bc_vec_top(&p->results);
-		if (var) n = bc_vec_item_rev(bc_program_vec(p, ptr->d.loc.loc, t), 1);
-	}
+
+	ptr = bc_vec_top(&p->results);
+
+	s = bc_program_type_match(ptr, t);
+	if (BC_ERR(s)) return s;
+
+	if (last) s = bc_program_num(p, ptr, &n);
+	else if (var) n = bc_vec_item_rev(bc_program_vec(p, ptr->d.loc.loc, t), 1);
 #else // BC_ENABLED
 	s = bc_program_operand(p, &ptr, &n, 0);
 #endif // BC_ENABLED
 
 	if (BC_ERR(s)) return s;
-
-#if BC_ENABLED
-	s = bc_program_type_match(ptr, t);
-	if (BC_ERR(s)) return s;
-#endif // BC_ENABLED
 
 	vec = bc_program_vec(p, idx, t);
 

commit ce1b5a57153d4950903f2db15501b64fba3559f2

Fix a bug I introduced for dc in a recent fix
This bug was introduced in 2b00094faf54d8e1a8009b275565f34ea95fe5fc. It
made it so dc was not doing the right thing when copying a variable.

src/program.c
@@ -725,14 +725,20 @@ static BcStatus bc_program_copyToVar(BcProgram *p, size_t idx,
 	bool var = (t == BC_TYPE_VAR);
 
 #if BC_ENABLED
+#if DC_ENABLED
+	if (!BC_IS_BC) s = bc_program_operand(p, &ptr, &n, 0);
+	else
+#endif // DC_ENABLED
+	{
+		ptr = bc_vec_top(&p->results);
 
-	ptr = bc_vec_top(&p->results);
+		s = bc_program_type_match(ptr, t);
+		if (BC_ERR(s)) return s;
 
-	s = bc_program_type_match(ptr, t);
-	if (BC_ERR(s)) return s;
-
-	if (last) s = bc_program_num(p, ptr, &n);
-	else if (var) n = bc_vec_item_rev(bc_program_vec(p, ptr->d.loc.loc, t), 1);
+		if (last) s = bc_program_num(p, ptr, &n);
+		else if (var)
+			n = bc_vec_item_rev(bc_program_vec(p, ptr->d.loc.loc, t), 1);
+	}
 #else // BC_ENABLED
 	s = bc_program_operand(p, &ptr, &n, 0);
 #endif // BC_ENABLED