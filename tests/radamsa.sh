81d7b1122470e1b751689e4e7e946559018893ee
Gavin Howard <yzena.tech@gmail.com>
2020-05-14 08:03:09 -0600

    Change radamsa.sh to be able to fuzz like afl

--- tests/radamsa.sh
+++ tests/radamsa.sh
@@ -51,9 +51,29 @@ getentry() {
 
 script="$0"
 
+if [ "$#" -lt 1 ]; then
+	printf 'usage: %s dir\n' "$0"
+	exit 1
+fi
+
+d="$1"
+shift
+
 dir=$(dirname "$script")
 
+. "$dir/../functions.sh"
+
 bcdir="$dir/../bin"
+
+if [ "$d" = "bc" ]; then
+	inputs="$dir/../../inputs"
+	opts="-lq"
+elif [ "$d" = "dc" ]; then
+	inputs="$dir/../../inputs_dc"
+	opts="-x"
+else
+	err_exit "wrong type of executable" 1
+fi
 
 export ASAN_OPTIONS="abort_on_error=1"
 
@@ -61,26 +81,40 @@ entries=$(cat "$dir/radamsa.txt")
 
 IFS=$'\n'
 
-while [ 1 ]; do
+go=1
 
-	entry=$(cat -- "$dir/radamsa.txt" | getentry)
-	items=$(printf '%s\n' "$entry" | radamsa -n 10)
+while [ "$go" -ne 0 ]; do
 
-	printf '%s\n' "$items"
+	if [ "$d" = "bc" ]; then
 
-	for i in `seq 1 10`; do
+		entry=$(cat -- "$dir/radamsa.txt" | getentry)
+		items=$(printf '%s\n' "$entry" | radamsa -n 10)
 
-		item=$(printf '%s\n' "$items" | getentry "$i")
+		printf '%s\n' "$items"
 
-		export BC_ENV_ARGS="$item"
-		echo 'halt' | bin/bc
+		for i in `seq 1 10`; do
+
+			item=$(printf '%s\n' "$items" | getentry "$i")
+
+			export BC_ENV_ARGS="$item"
+			echo 'halt' | "$bcdir/$d"
+			err=$?
+
+			checkcrash "$d" "$err" "radamsa env args: \"$item\""
+		done
+
+	fi
+
+	f=$(ls "$inputs" | getentry)
+	l=$(cat "$inputs/$f" | wc -l)
+	ll=$(printf '%s^2\n' "$l" | bc)
+
+	for i in $(seq 1 2); do
+		data=$(cat "$inputs/$f" | radamsa -n 1)
+		printf '%s\n' "$data" > "$dir/../.log_${d}_test.txt"
+		printf '%s\n' "$data" | timeout -s SIGTERM 5 "$bcdir/$d" "$opts" > /dev/null
 		err=$?
-
-		if [ "$err" -gt 127 ]; then
-			printf 'bc crashed with error %d on the following:\n' "$err"
-			printf '%s\n' "$item"
-			exit 1
-		fi
+		checkcrash "$d" "$err" "radamsa stdin"
 	done
 
 done
