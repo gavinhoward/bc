2c628a589f44296280785db4792d18666acacbe1
Gavin Howard <yzena.tech@gmail.com>
2020-05-14 12:39:39 -0600

    Fix style

--- src/program.c
+++ src/program.c
@@ -1257,8 +1257,10 @@ static BcStatus bc_program_builtin(BcProgram *p, uchar inst) {
 #if DC_ENABLED
 				if (!BC_PROG_NUM(opd, num)) {
 					size_t idx;
+					char *str;
 					idx = opd->t == BC_RESULT_STR ? opd->d.loc.loc : num->scale;
-					val = (BcBigDig) strlen(*((char**) bc_vec_item(p->strs, idx)));
+					str = *((char**) bc_vec_item(p->strs, idx));
+					val = (BcBigDig) strlen(str);
 				}
 				else
 #endif // DC_ENABLED
