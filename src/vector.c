0beead5bcbc50a11625f3406beff30fe5b6cc15e
Gavin Howard <yzena.tech@gmail.com>
2020-05-22 19:47:00 -0600

    Make bc_vec_pushByte() more efficient

--- src/vector.c
+++ src/vector.c
@@ -107,7 +107,9 @@ void bc_vec_push(BcVec *restrict v, const void *data) {
 
 void bc_vec_pushByte(BcVec *restrict v, uchar data) {
 	assert(v != NULL && v->size == sizeof(uchar));
-	bc_vec_push(v, &data);
+	if (v->len == v->cap) bc_vec_grow(v, 1);
+	v->v[v->len] = data;
+	v->len += 1;
 }
 
 void bc_vec_pushIndex(BcVec *restrict v, size_t idx) {
