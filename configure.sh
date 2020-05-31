586baec533785a01341bb54a57c8db13effc73c0
Gavin Howard <yzena.tech@gmail.com>
2020-05-30 23:07:53 -0600

    Remove signals test infrastructure

--- configure.sh
+++ configure.sh
@@ -523,12 +523,10 @@ executable="BC_EXEC"
 bc_test="@tests/all.sh bc $extra_math 1 $generate_tests 0 \$(BC_EXEC)"
 bc_time_test="@tests/all.sh bc $extra_math 1 $generate_tests 1 \$(BC_EXEC)"
 bc_history_test="@tests/history.sh bc \$(BC_EXEC)"
-bc_signals_test="@tests/signals.sh bc \$(BC_EXEC)"
 
 dc_test="@tests/all.sh dc $extra_math 1 $generate_tests 0 \$(DC_EXEC)"
 dc_time_test="@tests/all.sh dc $extra_math 1 $generate_tests 1 \$(DC_EXEC)"
 dc_history_test="@tests/history.sh dc \$(DC_EXEC)"
-dc_signals_test="@tests/signals.sh dc \$(DC_EXEC)"
 
 timeconst="@tests/bc/timeconst.sh tests/bc/scripts/timeconst.bc \$(BC_EXEC)"
 
@@ -561,7 +559,6 @@ if [ "$bc_only" -eq 1 ]; then
 	dc_test="@printf 'No dc tests to run\\\\n'"
 	dc_time_test="@printf 'No dc tests to run\\\\n'"
 	dc_history_test="@printf 'No dc history tests to run\\\\n'"
-	dc_signals_test="@printf 'No dc signals tests to run\\\\n'"
 	vg_dc_test="@printf 'No dc tests to run\\\\n'"
 
 	install_prereqs=" install_bc_manpage"
@@ -584,7 +581,6 @@ elif [ "$dc_only" -eq 1 ]; then
 	bc_test="@printf 'No bc tests to run\\\\n'"
 	bc_time_test="@printf 'No bc tests to run\\\\n'"
 	bc_history_test="@printf 'No bc history tests to run\\\\n'"
-	bc_signals_test="@printf 'No bc signals tests to run\\\\n'"
 	vg_bc_test="@printf 'No bc tests to run\\\\n'"
 
 	timeconst="@printf 'timeconst cannot be run because bc is not built\\\\n'"
@@ -914,12 +910,10 @@ contents=$(replace "$contents" "EXEC" "$executable")
 contents=$(replace "$contents" "BC_TEST" "$bc_test")
 contents=$(replace "$contents" "BC_TIME_TEST" "$bc_time_test")
 contents=$(replace "$contents" "BC_HISTORY_TEST" "$bc_history_test")
-contents=$(replace "$contents" "BC_SIGNALS_TEST" "$bc_signals_test")
 
 contents=$(replace "$contents" "DC_TEST" "$dc_test")
 contents=$(replace "$contents" "DC_TIME_TEST" "$dc_time_test")
 contents=$(replace "$contents" "DC_HISTORY_TEST" "$dc_history_test")
-contents=$(replace "$contents" "DC_SIGNALS_TEST" "$dc_signals_test")
 
 contents=$(replace "$contents" "VG_BC_TEST" "$vg_bc_test")
 contents=$(replace "$contents" "VG_DC_TEST" "$vg_dc_test")
