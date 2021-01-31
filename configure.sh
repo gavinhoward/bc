8b9f8084a004548f5981eaee6548d69960ef0f27
Gavin Howard <yzena.tech@gmail.com>
2021-01-31 11:28:08 -0700

    Tweak build output

--- configure.sh
+++ configure.sh
@@ -1204,7 +1204,7 @@ IFS=$'\n'
 
 for f in $src_files; do
 	o=$(replace_ext "$f" "c" "o")
-	SRC_TARGETS=$(printf '%s\n\n%s: %s %s\n\t$(CC) $(CFLAGS) -o "%s" -c "%s"\n' \
+	SRC_TARGETS=$(printf '%s\n\n%s: %s %s\n\t$(CC) $(CFLAGS) -o %s -c %s\n' \
 		"$SRC_TARGETS" "$o" "$headers" "$f" "$o" "$f")
 done
 
