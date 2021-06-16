bd57da06a8472efa7f68d5aee10718616d82ff16
Gavin Howard <gavin@yzena.com>
2021-06-16 14:54:31 -0600

    Fix dc stream
    It turns out that GNU bc was doing it wrong. I ran the GNU bc output
    through xxd, and then I really looked at it compared to the actual dc
    code.
    
    Turns out that it was flat wrong with values above 255. Ouch.
    
    Signed-off-by: Gavin Howard <gavin@yzena.com>

--- src/num.c
+++ src/num.c
@@ -1728,16 +1728,6 @@ static void bc_num_putchar(int c, bool bslash) {
 	bc_vm_putchar(c, bc_flush_save);
 }
 
-#if DC_ENABLED && !BC_ENABLE_LIBRARY
-static void bc_num_printChar(size_t n, size_t len, bool rdx, bool bslash) {
-	BC_UNUSED(rdx);
-	BC_UNUSED(len);
-	BC_UNUSED(bslash);
-	assert(len == 1);
-	bc_vm_putchar((uchar) n, bc_flush_save);
-}
-#endif // DC_ENABLED && !BC_ENABLE_LIBRARY
-
 static void bc_num_printDigits(size_t n, size_t len, bool rdx, bool bslash) {
 
 	size_t exp, pow;
@@ -2127,7 +2117,8 @@ static void bc_num_printBase(BcNum *restrict n, BcBigDig base, bool newline) {
 
 #if DC_ENABLED && !BC_ENABLE_LIBRARY
 void bc_num_stream(BcNum *restrict n, BcBigDig base) {
-	bc_num_printNum(n, base, 1, bc_num_printChar, false);
+	BC_UNUSED(base);
+	bc_vm_putchar((uchar) (n->num[0] & 255), bc_flush_save);
 }
 #endif // DC_ENABLED && !BC_ENABLE_LIBRARY
 
